c857cefeb68ecdfb2a0667fff5c351c087e633ec
ywanzhou <ywanzhou@foxmail.com>
2022-06-22 16:34:54 +0800

    feat: 配置别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import type { ConfigEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({ mode }: ConfigEnv) => {
+  return {
+    resolve: {
+      alias: {
+        '/@': resolve(__dirname, 'src'),
+      },
+      extensions: ['.js', '.json', '.ts', '.vue'], // 使用路径别名时想要省略的后缀名，可以自己 增减
+    },
+
+    /* more config */
+    plugins: [vue()],
+  }
 })
