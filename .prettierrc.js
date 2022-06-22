a3d8e5381cd7a139e65e703c33cb1484a308845d
ywanzhou <ywanzhou@foxmail.com>
2022-06-22 15:59:33 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // 结尾分号
+  semi: false,
+  // 单引号
+  singleQuote: true,
+  // 一行80字符
+  printWidth: 80,
+  // 尾逗号
+  trailingComma: 'all',
+  // 箭头函数的括号
+  arrowParens: 'avoid',
+  // 换行符
+  endOfLine: 'lf',
+}
